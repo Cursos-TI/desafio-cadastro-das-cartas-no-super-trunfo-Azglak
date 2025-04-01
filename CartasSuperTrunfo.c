0f2db7bbe138b3c7fdf197db566355f724dafc25
Azglak <gmps023@gmail.com>
2025-04-01 10:34:14 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,42 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
+struct Cidade {
+    char Nome;
+    int População;
+    float Area;
+    float PIB;
+    int NPTuristicos;
+};
+
 int main() {
+    struct Cidade cidade1;
+    struct Cidade cidade2;
+    
+    printf("Olá, bem-vindo ao Super Trunfo Cidades!\nPor favor, insira os dados da primeira cidade.\n");
+    printf("População:\n");
+    scanf("%d", &cidade1.População);
+    printf("Área:\n");
+    scanf("%f", &cidade1.Area);
+    printf("PIB:\n");
+    scanf("%f", &cidade1.PIB);
+    printf("Número de pontos turísticos:\n");
+    scanf("%d", &cidade1.NPTuristicos);
+
+    printf("Ótimo.\nAgora insira os da segunda cidade.\n");
+    printf("População:\n");
+    scanf("%d", &cidade2.População);
+    printf("Área:\n");
+    scanf("%f", &cidade2.Area);
+    printf("PIB:\n");
+    scanf("%f", &cidade2.PIB);
+    printf("Número de pontos turísticos:\n");
+    scanf("%d", &cidade2.NPTuristicos);
+    
+    printf("A primeira cidade é o lar de %d pessoas, tem %f de área, %f de PIB e possui %d pontos turísticos\n",cidade1.População, cidade1.Area, cidade1.PIB, cidade1.NPTuristicos);
+    
+    printf("Já a segunda é populada por %d pessoas, tem %f de área, %f de PIB e possui %d pontos turísticos", cidade2.População, cidade2.Area, cidade2.PIB, cidade2.NPTuristicos);
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
