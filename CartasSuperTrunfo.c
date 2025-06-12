8e3d0aaf1f449858357cc8a04a7108389bcdb68f
Azglak <gmps023@gmail.com>
2025-06-11 21:56:36 -0300

    Update CartasSuperTrunfo.c
    Preparações para a próxima atividade:
    
    Função de nomes consertada e implementada.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 struct Cidade {
-    char Nome;
     int População;
     float Area;
     float PIB;
     int NPTuristicos;
+    char Estado;
+    char Nome[];
 };
 
 int main() {
@@ -13,6 +14,11 @@ int main() {
     struct Cidade cidade2;
     
     printf("Olá, bem-vindo ao Super Trunfo Cidades!\nPor favor, insira os dados da primeira cidade.\n");
+    printf("Nome:\n");
+    scanf(" %s", &cidade1.Nome);
+    getchar();
+    printf("Estado:\n");
+    scanf(" %c", &cidade1.Estado);
     printf("População:\n");
     scanf("%d", &cidade1.População);
     printf("Área:\n");
@@ -23,6 +29,11 @@ int main() {
     scanf("%d", &cidade1.NPTuristicos);
 
     printf("Ótimo.\nAgora insira os da segunda cidade.\n");
+    printf("Nome:\n");
+    scanf("%s", &cidade2.Nome);
+    getchar();
+    printf("Estado:\n");
+    scanf(" %c", &cidade2.Estado);
     printf("População:\n");
     scanf("%d", &cidade2.População);
     printf("Área:\n");
@@ -32,9 +43,9 @@ int main() {
     printf("Número de pontos turísticos:\n");
     scanf("%d", &cidade2.NPTuristicos);
     
-    printf("A primeira cidade é o lar de %d pessoas, tem %f de área, %f de PIB e possui %d pontos turísticos, além de possuir %f de densidade demográfica e %f de PIB per capta!\n",cidade1.População, cidade1.Area, cidade1.PIB, cidade1.NPTuristicos, cidade1.População/cidade1.Area, cidade1.PIB/cidade1.População);
+    printf("%s é o lar de %d pessoas, tem %f de área, %f de PIB e possui %d pontos turísticos, além de possuir %f de densidade demográfica e %f de PIB per capta!\n", cidade1.Nome, cidade1.População, cidade1.Area, cidade1.PIB, cidade1.NPTuristicos, cidade1.População/cidade1.Area, cidade1.PIB/cidade1.População);
     
-    printf("Já a segunda é populada por %d pessoas, tem %f de área, %f de PIB e possui %d pontos turísticos, além de possuir %f de densidade demográfica e %f de PIB per capta!", cidade2.População, cidade2.Area, cidade2.PIB, cidade2.NPTuristicos, cidade2.População/cidade2.Area, cidade2.PIB/cidade2.População);
-
+    printf("Já %s é populada por %d pessoas, tem %f de área, %f de PIB e possui %d pontos turísticos, além de possuir %f de densidade demográfica e %f de PIB per capta!\n\n",cidade2.Nome, cidade2.População, cidade2.Area, cidade2.PIB, cidade2.NPTuristicos, cidade2.População/cidade2.Area, cidade2.PIB/cidade2.População);
+    
     return 0;
 }
